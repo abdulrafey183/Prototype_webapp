e24a8501a6e1698b5bc1b3d2619c87183ad0397a
Anum Mujahid <amujahid.bese18seecs@student.nust.edu.pk>
2021-05-27 11:17:02 +0500

    style(): fix style

--- application/static/js/displaypage.js
+++ application/static/js/displaypage.js
@@ -1,4 +1,4 @@
-let tabs = ['buyer', 'plot', 'CA', 'ET', 'deal',  'employee'];
+let tabs = ['buyer', 'plot', 'CA', 'ET', 'deal', 'employee'];
 
 function clicked(name) {
   //Turning on the cliked div
@@ -20,7 +20,10 @@ function make_plot_card(plot) {
     "' aria-expanded='true' aria-controls='collapseBuyer" +
     plot.id +
     "'>" +
-    "PLOT# "+ plot.id + " - " + plot.address +
+    'PLOT# ' +
+    plot.id +
+    ' - ' +
+    plot.address +
     "</button></h2></div><div id='collapseBuyer" +
     plot.id +
     "' class='collapse' aria-labelledby='headingOne' data-parent='#accordionExample'><div class='card-body'>" +
@@ -35,28 +38,34 @@ function make_plot_card(plot) {
     '</td></tr>';
 
   if (!plot.price)
-    str += "<tr class='text-dark-3'><th scope='row'>Price</th><td><span style='color: red;'>Not Set</span></td></tr>";
+    str +=
+      "<tr class='text-dark-3'><th scope='row'>Price</th><td><span style='color: red;'>Not Set</span></td></tr>";
   else
-    str +=  "<tr class='text-dark-3'><th scope='row'>Price</th><td>" +
-            plot.price +
-            "</td></tr>";
+    str +=
+      "<tr class='text-dark-3'><th scope='row'>Price</th><td>" +
+      plot.price +
+      '</td></tr>';
 
-  str +=    "<tr class='text-dark-3'><th scope='row'>Size</th><td>" +
-            plot.size +
-            "</td></tr>";
+  str +=
+    "<tr class='text-dark-3'><th scope='row'>Size</th><td>" +
+    plot.size +
+    '</td></tr>';
 
   if (plot.deal)
-    str +=  "<tr class='text-dark-3'><th scope='row'>Deal</th><td><a href='/deal/" +
-            plot.deal.id +
-            "'>" +
-            "Deal# " + plot.deal.id +
-            "</a></td></tr>";
-  
+    str +=
+      "<tr class='text-dark-3'><th scope='row'>Deal</th><td><a href='/deal/" +
+      plot.deal.id +
+      "'>" +
+      'Deal# ' +
+      plot.deal.id +
+      '</a></td></tr>';
 
-  str +=    "<tr class='text-dark-3'><td scope='row'>" +
-            "<span><a href='/plot/" + plot.id + "''>Show Details</a></span></td><td></td>" +
-            "</tr>"
-             
+  str +=
+    "<tr class='text-dark-3'><td scope='row'>" +
+    "<span><a href='/plot/" +
+    plot.id +
+    "''>Show Details</a></span></td><td></td>" +
+    '</tr>';
 
   return (
     str + '</tbody></table></div></div></div></section></div></div></div></div>'
@@ -64,12 +73,13 @@ function make_plot_card(plot) {
 }
 
 function make_buyer_card(buyer) {
-  
   var deal_ids = '';
-  if(buyer.deals != null)
-    for(var deal of buyer.deals){ deal_ids += ' <a href="/deal/' + deal.id + '">Deal#' + deal.id + '</a><br>'; }
-  else
-    deal_ids = 'None';
+  if (buyer.deals != null)
+    for (var deal of buyer.deals) {
+      deal_ids +=
+        ' <a href="/deal/' + deal.id + '">Deal#' + deal.id + '</a><br>';
+    }
+  else deal_ids = 'None';
 
   let str =
     "<div class='accordion' id='accordionExample'><div class='card'><div class='card-header' id='headingOne'><h2 class='mb-0'><button class='btn btn-link btn-block text-left' type='button' data-toggle='collapse' data-target='#collapseBuyer" +
@@ -82,7 +92,9 @@ function make_buyer_card(buyer) {
     buyer.id +
     "' class='collapse' aria-labelledby='headingOne' data-parent='#accordionExample'><div class='card-body'>" +
     "<section class='my-3'><div class='container'><div class='row'><div class='col-12'><table class='table mt-4'><tbody<tr class='text-dark-3'><th scope='row'>Name</th><td>" +
-    "<a href='/buyer/" + buyer.id + "'>" +
+    "<a href='/buyer/" +
+    buyer.id +
+    "'>" +
     buyer.person.name +
     "</a></td></tr><tr class='text-dark-3'><th scope='row'>CNIC</th><td>" +
     buyer.person.cnic +
@@ -92,49 +104,58 @@ function make_buyer_card(buyer) {
     buyer.person.email +
     '</td></tr>';
 
-    if(deal_ids != 'None'){
-      str += '<tr class="text-dark-3"><th scope="row">Deals</th><td>' + deal_ids + '</td></tr>';
-    }
-    str += '</tbody></table></div></div></div></section>' +
-    '</div></div></div></div>';
+  if (deal_ids != 'None') {
+    str +=
+      '<tr class="text-dark-3"><th scope="row">Deals</th><td>' +
+      deal_ids +
+      '</td></tr>';
+  }
+  str +=
+    '</tbody></table></div></div></div></section>' + '</div></div></div></div>';
 
   return str;
 }
 
 function make_deal_card(deal) {
-  
   let str =
     "<div class='accordion' id='accordionExample'><div class='card'><div class='card-header' id='headingOne'><h2 class='mb-0'><button class='btn btn-link btn-block text-left' type='button' data-toggle='collapse' data-target='#collapseBuyer" +
     deal.id +
     "' aria-expanded='true' aria-controls='collapseBuyer" +
     deal.id +
     "'>" +
-    "DEAL# " + deal.id +
+    'DEAL# ' +
+    deal.id +
     "</button></h2></div><div id='collapseBuyer" +
     deal.id +
     "' class='collapse' aria-labelledby='headingOne' data-parent='#accordionExample'><div class='card-body'>" +
-    "<section class='my-3'><div class='container'><div class='row'><div class='col-12'><table class='table mt-4'><tbody><tr class='text-dark-3'><td scope='row'><a href='/add/transaction/receivepayment/" + deal.id + "'>Recieve Payment</a></td><td>" +
-    "</td></tr>" +
+    "<section class='my-3'><div class='container'><div class='row'><div class='col-12'><table class='table mt-4'><tbody><tr class='text-dark-3'><td scope='row'><a href='/add/transaction/receivepayment/" +
+    deal.id +
+    "'>Recieve Payment</a></td><td>" +
+    '</td></tr>' +
     "<tr class='text-dark-3'><th scope='row'>Date of Signing</th><td>" +
     deal.signing_date +
     "</td></tr><tr class='text-dark-3'><th scope='row'>Respective Plot</th><td>" +
     deal.plot_id +
     "</td></tr><tr class='text-dark-3'><th scope='row'>Respective Buyer</th><td>" +
-    "<a href='/buyer/" + deal.buyer_id + "'><div class='buyer-" + deal.buyer_id + "'>" +
-    "</div></a>" +
+    "<a href='/buyer/" +
+    deal.buyer_id +
+    "'><div class='buyer-" +
+    deal.buyer_id +
+    "'>" +
+    '</div></a>' +
     "</td></tr><tr class='text-dark-3'><td><a href='/deal/" +
     deal.id +
     "'>Show Details</a></td><td></td></tr></tbody></table></div></div></div></section>";
 
-  return str; 
+  return str;
 }
 function make_CA_card(CA) {
-
   var deal_ids = '';
-  if(CA.deals != null)
-    for(var deal of CA.deals){ deal_ids += ' <a href="/deal/' + deal.id + '">' + deal.id + '</a>'; }
-  else
-    deal_ids = 'None';
+  if (CA.deals != null)
+    for (var deal of CA.deals) {
+      deal_ids += ' <a href="/deal/' + deal.id + '">' + deal.id + '</a>';
+    }
+  else deal_ids = 'None';
 
   let str =
     "<div class='accordion' id='accordionExample'><div class='card'><div class='card-header' id='headingOne'><h2 class='mb-0'><button class='btn btn-link btn-block text-left' type='button' data-toggle='collapse' data-target='#collapseBuyer" +
@@ -147,21 +168,26 @@ function make_CA_card(CA) {
     CA.person.id +
     "' class='collapse' aria-labelledby='headingOne' data-parent='#accordionExample'><div class='card-body'>" +
     "<section class='my-3'><div class='container'><div class='row'><div class='col-12'><table class='table mt-4'><tbody><tr class='text-dark-3'><th scope='row'>Name</th><td>" +
-    "<a href='/agent/" + CA.person.id + "'>" +
+    "<a href='/agent/" +
+    CA.person.id +
+    "'>" +
     CA.person.name +
     "</a></td></tr><tr class='text-dark-3'><th scope='row'>CNIC</th><td>" +
     CA.person.cnic +
     "</td></tr><tr class='text-dark-3'><th scope='row'>Phone</th><td>" +
     CA.person.phone +
     "</td></tr><tr class='text-dark-3'><th scope='row'>Email</th><td>" +
-    CA.person.email + "</td></tr>";
+    CA.person.email +
+    '</td></tr>';
 
-  if(deal_ids != 'None'){
-    str += "<tr class='text-dark-3'><th scope='row'>Deals</th><td>" + deal_ids + "</td></tr>";
+  if (deal_ids != 'None') {
+    str +=
+      "<tr class='text-dark-3'><th scope='row'>Deals</th><td>" +
+      deal_ids +
+      '</td></tr>';
   }
   str +=
-    '</tbody></table></div></div></div></section>' +
-    '</div></div></div></div>';
+    '</tbody></table></div></div></div></section>' + '</div></div></div></div>';
 
   return str;
 }
@@ -184,12 +210,13 @@ function make_ET_card(ET) {
     "</a></td></tr><tr class='text-dark-3'><th scope='row'>Type</th><td>" +
     ET.name +
     "</td></tr></tbody></table><a href='/expenditure/" +
-    ET.id + "'>Show Details</a></div></div></div></section>" +
+    ET.id +
+    "'>Show Details</a></div></div></div></section>" +
     '</div></div></div></div>';
 
   return str;
 }
- 
+
 function make_employee_card(employee) {
   let str =
     "<div class='accordion' id='accordionExample'><div class='card'><div class='card-header' id='headingOne'><h2 class='mb-0'><button class='btn btn-link btn-block text-left' type='button' data-toggle='collapse' data-target='#collapseBuyer" +
@@ -202,31 +229,39 @@ function make_employee_card(employee) {
     employee.id +
     "' class='collapse' aria-labelledby='headingOne' data-parent='#accordionExample'><div class='card-body'>" +
     "<section class='my-3'><div class='container'><div class='row'><div class='col-12'><table class='table mt-4'><tbody>" +
-    "<tr class='text-dark-3'>" + 
-      "<th scope='row'>Name</th>" + 
-      "<td><a href='/employee/" + employee.id +"'>" + employee.person.name + "</a></td>" + 
-    "</tr>";
+    "<tr class='text-dark-3'>" +
+    "<th scope='row'>Name</th>" +
+    "<td><a href='/employee/" +
+    employee.id +
+    "'>" +
+    employee.person.name +
+    '</a></td>' +
+    '</tr>';
 
-  if(employee.rank == 1){
-    str +=  "<tr class='text-dark-3'>" + 
-              "<th scope='row'>Type</th>" + 
-              "<td>User</td>" + 
-            "</tr>" +
-            "<tr class='text-dark-3'>" + 
-              "<th scope='row'>Email ID</th>" + 
-              "<td>" + employee.person.email + "</td>" + 
-            "</tr>";
-  }
-  else if(employee.rank == 2){
-    str +=  "<tr class='text-dark-3'>" + 
-              "<th scope='row'>Type</th>" + 
-              "<td>Employee</td>" + 
-            "</tr>";
+  if (employee.rank == 1) {
+    str +=
+      "<tr class='text-dark-3'>" +
+      "<th scope='row'>Type</th>" +
+      '<td>User</td>' +
+      '</tr>' +
+      "<tr class='text-dark-3'>" +
+      "<th scope='row'>Email ID</th>" +
+      '<td>' +
+      employee.person.email +
+      '</td>' +
+      '</tr>';
+  } else if (employee.rank == 2) {
+    str +=
+      "<tr class='text-dark-3'>" +
+      "<th scope='row'>Type</th>" +
+      '<td>Employee</td>' +
+      '</tr>';
   }
 
-  return str + "</tbody></table></div></div></div></section></div></div></div></div>";
+  return (
+    str + '</tbody></table></div></div></div></section></div></div></div></div>'
+  );
 }
-
 
 function inject_div(name, list) {
   let str = '';
@@ -234,8 +269,7 @@ function inject_div(name, list) {
   for (let element of list) {
     str = make_card(element);
     $('#' + name + '-info').append(str);
-    if(name=='deal')
-      getbuyer(element.buyer_id);
+    if (name == 'deal') getbuyer(element.buyer_id);
   }
 }
 
@@ -249,8 +283,8 @@ function getall(name) {
 function getbuyer(id) {
   $.post('/rest/' + 'Buyer' + '/' + id, function (data) {
     for (let element of data.json_list) {
-        $('.buyer-' + id).text(element.person.name);
-      }
+      $('.buyer-' + id).text(element.person.name);
+    }
   });
 }
 
